cc09505b119821a0bb72666182c56f80cc8eed5a
Kyle Dettloff <kyle.dettloff@noaa.gov>
2026-07-24 08:22:11 -0400

    Refactor Trip Matching Code for Clarity and Optimization

--- TripMatchingCode.R
+++ TripMatchingCode.R
@@ -52,7 +52,7 @@ true_matches <- matched_pool %>%
   # filter to high vessel similarities and exclude vessel name matches when "UNNAMED"
   filter(VslNum_Sim >= ves_sim_thres | VslName_Sim >= ves_sim_thres & Log_Vessel_Name != "UNNAMED" & Surv_Vessel_Name != "UNNAMED") %>%
   # keep record with highest time similarity after prioritizing same site when multiple matches for same vessel on same date
-  group_by(Surv_Survey_RowID) %>% arrange(desc(Site_Sim), desc(Time_Sim)) %>% slice(1) %>% %>% ungroup() %>%
+  group_by(Surv_Survey_RowID) %>% arrange(desc(Site_Sim), desc(Time_Sim)) %>% slice(1) %>% ungroup() %>%
   mutate(is_match = 1)
 
 eval_df <- matched_pool %>%
